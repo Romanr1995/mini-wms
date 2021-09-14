69e06bf5e237ae7ed5e266f9aa099a5f5f9930ff
Roman Nozdrachev <roman.nozdracheff@yandex.ru>
2021-09-14 12:43:38 +0300

    Поправил комментарии

--- src/main/java/ru/nozdrachev/miniwms/service/UnitConversionService.java
+++ src/main/java/ru/nozdrachev/miniwms/service/UnitConversionService.java
@@ -8,21 +8,21 @@ public interface UnitConversionService {
 
     /**
      *Метод calculateBaseCnt используется для перевода количества товара из альтернативной единицы измерения
-     * в количество товара в базовой единице измерения
-     * @param productName -> наименование товара
-     * @param alternativeCnt -> количество товара в альтернативных единицах
-     * @param altUnit -> текущая альтернативная единица хранения товара
-     * @return -> количество товара в базовой единице
+     * в количество товара в базовой единице измерения.
+     * @param productName -> наименование товара.
+     * @param alternativeCnt -> количество товара в альтернативных единицах.
+     * @param altUnit -> текущая альтернативная единица хранения товара.
+     * @return -> количество товара в базовой единице.
      */
     BigDecimal calculateBaseCnt(String productName, BigDecimal alternativeCnt, UnitOfMeasurement altUnit);
 
     /**
      *Метод calculateAltCnt используется для перевода количества товара в базовой единице измерения
-     *в количество товара в альтернативной единице измерения
-     * @param productName -> наименование товара
-     * @param baseCnt -> количество товара в базовой единице
-     * @param altUnit -> единица хранения товара в которую необходимо перевести
-     * @return -> количество товара в альтернативной единице
+     *в количество товара в альтернативной единице измерения.
+     * @param productName -> наименование товара.
+     * @param baseCnt -> количество товара в базовой единице.
+     * @param altUnit -> единица хранения товара в которую необходимо перевести.
+     * @return -> количество товара в альтернативной единице.
      */
     BigDecimal calculateAltCnt(String productName, BigDecimal baseCnt, UnitOfMeasurement altUnit);
 }
